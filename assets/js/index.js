449d6b2bdd83e314b4928b51e37efe482257f1a6
Sunny17082 <sunnyprasad1708@gmail.com>
2023-08-30 00:13:18 +0530

    Added delet user functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,22 @@ $("#update_user").submit(function(event) {
     });
 
 });
+
+if(window.location.pathname == "/") {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id");
+
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`,
+            "method": "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")) {
+            $.ajax(request).done(function(response) {
+                alert("Data deleted successfully");
+                location.reload();
+            });
+        }
+    });
+}
