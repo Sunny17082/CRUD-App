048d1f030894efe2aa872618e3d25eb3785d6d92
Sunny17082 <sunnyprasad1708@gmail.com>
2023-08-29 22:26:50 +0530

    Added GET method for single user

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -31,15 +31,33 @@ exports.create = (req, res) => {
 
 // retrieve and return all users/retrive and return a single user
 exports.find = (req, res) => {
-    Userdb.find()
-    .then(user => {
-        res.send(user);
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message || "Error occured while retriving user information"
+    if(req.query.id) {
+        const id = req.query.id;
+
+        Userdb.findById(id)
+        .then(data => {
+            if(!data) {
+                res.status(404).send({message: `Not found user with id ${id}`});
+            } else {
+                res.send(data);
+            }
+        })
+        .catch(err => {
+            res.staus(500).send({
+                message: `Error retrieving user with id ${id}`
+            })
+        })
+    } else {
+        Userdb.find()
+        .then(user => {
+            res.send(user);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Error occured while retriving user information"
+            });
         });
-    });
+    }
 }
 
 // Update a new identified user by user id
